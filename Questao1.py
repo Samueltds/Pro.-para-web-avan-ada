c8111fdd7fb0fa121653891c69bdbda6233cbcc5
Samueltds <josesamuelteixeira@gmail.com>
2017-05-24 19:21:15 -0300

    Primeira Questão
    Atraibuí valores para variaveis(Representando as letras da  1 questao) e mostrei o resultado de todas as letras

---
+++ Questao1.py
@@ -0,0 +1,42 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a=((2+3)-(5*8)/4)
+>>> b=((7**2)*(4/2))
+>>> c=25%5
+>>> d=(81**0.5)
+>>> e=10>11 and 11<12
+>>> f=(10*9)--(20|50*1|20)
+>>> g=(7--(2*3.5) and (False or True))
+>>> h=(7>2 or (7--(2*2)))
+>>> i=g
+>>> j=(17/2)%2
+>>> k=not(8<16)or(not(18<6))
+>>> l=(3<5)or(3>10)and(3%2==0)
+>>> m=(14!=3)and(3!=2)or(14==0)
+>>> a
+-5.0
+>>> b
+98.0
+>>> c
+0
+>>> d
+9.0
+>>> e
+False
+>>> f
+144
+>>> g
+True
+>>> h
+True
+>>> i
+True
+>>> j
+0.5
+>>> k
+True
+>>> l
+True
+>>> m
+True
+
