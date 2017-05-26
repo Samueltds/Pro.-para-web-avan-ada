9cf8ac709681c0a9031d255bb2077c222bc747ad
Samueltds <josesamuelteixeira@gmail.com>
2017-05-25 23:22:50 -0300

    Verdadeira questao 11
    Corrigindo a 11

--- Questao11.py
+++ Questao11.py
@@ -1,4 +1,4 @@
-n=1
-m=2
-p=m+n
-print("Isto é um teste %d %d %d"%(n,m,p))
+n1=int(input ("Digite o primeiro numero:"))
+n2=int(input ("Digite o segundo numero:"))
+n3=int(input ("Digite o terceiro numero:"))
+print("A soma do primeiro e segundo numero multiplicado pela soma do segundo pelo terceiro é igual a",(n1+n2)*(n2+n3))
