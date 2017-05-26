3fec4f0f1de3f2ee5dbb992326797f69bac8adb9
Samueltds <josesamuelteixeira@gmail.com>
2017-05-26 09:39:13 -0300

    Conseguir a 18

---
+++ Questao18.py
@@ -0,0 +1,4 @@
+a,b,c,d,e,x=int(input("Digite o valor do coeficiente a:")),int(input("Digite o valor do coeficiente b:")),int(input("Digite o valor do coeficiente c:")),int(input("Digite o valor do coeficiente d:")),int(input("Digite o valor do coeficiente e:")),int(input("Digite o valor de x :"))
+p=a*x**4+b*x**3+c*x**2+d*x+e
+print("O resultado desse polinomio é %d"%p)
+
