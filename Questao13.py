edb91a3c609ef2b0db8b428540d3aff9315ffd1a
Samueltds <josesamuelteixeira@gmail.com>
2017-05-25 23:46:59 -0300

    Questoes 13 e 14

---
+++ Questao13.py
@@ -0,0 +1,2 @@
+sal=int(input("Digite o valor do seu salario:"))
+print("O salario com um aumento de 15% é",sal*0.15+sal)
---
+++ Questao14.py
@@ -0,0 +1,5 @@
+A=int(input("Digite o primeiro valor:"))
+B=int(input("Digite o segundo valor:"))
+A1=B
+B1=A
+print("Os valores das variaveis antes da troca %d e %d , e o valores depois da troca %d e %d"%(A,B,A1,B1))
