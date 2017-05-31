804486caeaa37bb9121739c4a996289e29413dd9
Samueltds <josesamuelteixeira@gmail.com>
2017-05-31 11:13:16 -0300

    Primeira questao da pratica do Slide 3

---
+++ Questao1Pratica.py
@@ -0,0 +1,6 @@
+try:
+    n=int(input("Digite um numero, em inteiro:"))
+    n2=int(input("Digite um numero, em inteiro:"))
+    print("Os numeros são %d e %d" % (n, n2))
+except:
+    print("Erro você digitou o valor por extenso")
